5b58be0e9ee811a9aded2654b1912f3e7b6106ce
curtisbellamy <35937828+curtisbellamy@users.noreply.github.com>
2019-02-07 19:48:17 -0800

    Added connectivity.txt

--- main.cpp
+++ main.cpp
@@ -3,26 +3,52 @@
 #include "GooglePageRank.hpp"
 #include <vector>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
 int main() {
 
-    double arr[25] = {0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int input = 0;
+    vector<int> input2;
+    int index = 0;
 
-    matrix test(arr, 25);
+    ifstream infile;
+    infile.open("connectivity.txt");
 
-    GooglePageRank pr(arr, 25);
+    if(!(infile.is_open())){
+        cerr << "Unable to open file." << endl;
+        exit(1);
+    } else {
+        while(infile >> input){
+            input2[index] = input;
+            index++;
 
-    matrix importance = pr.importanceMatrix(test);
+        }
 
-    matrix teleport = pr.teleportationMatrix(test);
+    }
 
-    matrix transition = pr.transitionMatrix(importance, teleport);
+    for (int i = 0; i < input2.size(); ++i) {
+        cout << input2[i] << " ";
+    }
 
-    matrix markov = pr.markov(transition);
 
-    pr.print(markov);
+
+//    double arr[25] = {0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+//
+//    matrix test(arr, 25);
+//
+//    GooglePageRank pr(arr, 25);
+//
+//    matrix importance = pr.importanceMatrix(test);
+//
+//    matrix teleport = pr.teleportationMatrix(test);
+//
+//    matrix transition = pr.transitionMatrix(importance, teleport);
+//
+//    matrix markov = pr.markov(transition);
+//
+//    pr.print(markov);
 
     return 0;
 }
