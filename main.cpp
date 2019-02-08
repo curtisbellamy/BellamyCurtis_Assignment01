f52a5e7877c120a63eb2a07e9a70a4e8bfb8e05d
curtisbellamy <35937828+curtisbellamy@users.noreply.github.com>
2019-02-07 20:17:08 -0800

    Changed file path.

--- main.cpp
+++ main.cpp
@@ -9,46 +9,39 @@ using namespace std;
 
 int main() {
 
-    int input = 0;
+    int input;
     vector<int> input2;
-    int index = 0;
 
-    ifstream infile;
-    infile.open("connectivity.txt");
+    fstream infile("connectivity.txt");
 
     if(!(infile.is_open())){
         cerr << "Unable to open file." << endl;
         exit(1);
     } else {
         while(infile >> input){
-            input2[index] = input;
-            index++;
-
+            input2.push_back(input);
         }
-
     }
 
+    double arr[input2.size()];
     for (int i = 0; i < input2.size(); ++i) {
-        cout << input2[i] << " ";
+        arr[i] = input2[i];
     }
 
 
+    matrix test(arr, 25);
 
-//    double arr[25] = {0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-//
-//    matrix test(arr, 25);
-//
-//    GooglePageRank pr(arr, 25);
-//
-//    matrix importance = pr.importanceMatrix(test);
-//
-//    matrix teleport = pr.teleportationMatrix(test);
-//
-//    matrix transition = pr.transitionMatrix(importance, teleport);
-//
-//    matrix markov = pr.markov(transition);
-//
-//    pr.print(markov);
+    GooglePageRank pr(arr, 25);
+
+    matrix importance = pr.importanceMatrix(test);
+
+    matrix teleport = pr.teleportationMatrix(test);
+
+    matrix transition = pr.transitionMatrix(importance, teleport);
+
+    matrix markov = pr.markov(transition);
+
+    pr.print(markov);
 
     return 0;
 }
